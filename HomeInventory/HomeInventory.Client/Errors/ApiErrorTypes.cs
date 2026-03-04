4b04d0714b8dfe0f39e5a0aa9a498bad78d29375
Kapim <michal.kapinus@gmail.com>
2026-03-04 20:21:24 +0100

    feat(client): added InvalidReponse error type to ApiErrorTypes

--- HomeInventory/HomeInventory.Client/Errors/ApiErrorTypes.cs
+++ HomeInventory/HomeInventory.Client/Errors/ApiErrorTypes.cs
@@ -6,6 +6,6 @@ namespace HomeInventory.Client.Errors
 {
     public enum ApiErrorTypes
     {
-        Validation, Unauthorized, Forbidden, NotFound, Conflict, Server, Network, Unknown
+        Validation, Unauthorized, Forbidden, NotFound, Conflict, Server, Network, InvalidResponse, Unknown
     }
 }
